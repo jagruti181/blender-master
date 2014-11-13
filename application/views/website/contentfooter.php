227f99729315bb15fddf8ed610bb146cbde648e5
Tushar <Tushar@Tushars-MacBook-Pro.local>
2014-11-13 19:42:35 +0530

    II not Ii

--- application/views/website/contentfooter.php
+++ application/views/website/contentfooter.php
@@ -4,7 +4,7 @@
                 <p> <a href="<?php echo site_url('website/termscondition');?>">Terms &amp; Conditions</a>
                     <a>Privacy Policy</a> 
                 </p>
-                <p class="comp-text">Contact us: River (An Independent Creative Agency), 269 Udyog Vihar, Phase Ii, Gurgaon, Haryana © 2014 Blenders Pride Limited. All rights reserved</p>
+                <p class="comp-text">Contact us: River (An Independent Creative Agency), 269 Udyog Vihar, Phase II, Gurgaon, Haryana © 2014 Blenders Pride Limited. All rights reserved</p>
             </div>
         </div>
     </div>
