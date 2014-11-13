844dfc7d37830f47b335a220409f3377baf7fbed
Tushar <Tushar@Tushars-MacBook-Pro.local>
2014-11-13 18:15:58 +0530

    kicka kicka

--- application/views/website/sidemenu.php
+++ application/views/website/sidemenu.php
@@ -1,11 +1,11 @@
 <nav class="st-menu st-effect-11" id="menu-11">
     <h2>Menu</h2>
     <ul>
-        <li><a class="" href="<?php echo site_url('website/schedule'); ?>">EVENT SCHEDULE</a>
+        <li><a class="<?php if($page=="schedule") echo "active";?>" href="<?php echo site_url('website/schedule'); ?>">EVENT SCHEDULE</a>
         </li>
-        <li><a class="" href="<?php echo site_url('website/blenderstyle'); ?>">STYLE BLENDERS</a>
+        <li><a class="<?php if($page=="blenderstyle") echo "active";?>" href="<?php echo site_url('website/blenderstyle'); ?>">STYLE BLENDERS</a>
         </li>
-        <li><a class="" href="<?php echo site_url('website/invitelist'); ?>">INVITE LIST</a>
+        <li><a class="<?php if($page=="invitelist") echo "active";?>" href="<?php echo site_url('website/invitelist'); ?>">INVITE LIST</a>
         </li>
         <li><a class="" href="<?php echo base_url("")."archive";?>">BPFT 2013</a>
         </li>
