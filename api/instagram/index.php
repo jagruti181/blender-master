014ea6ebb18c1352b8fcef22b724a3ffa30305f6
wohlig <chintan@wohlig.com>
2014-11-15 00:09:51 +0530

    try insta

--- api/instagram/index.php
+++ api/instagram/index.php
@@ -22,7 +22,7 @@ $access_token = "43118528.6e367f9.ede6ba4052b84d0a97504b7c511790f4";
 if (1) {
   
    $max_id = $sinceid; 
-   $maxid = "&min_id=$max_id";
+   $maxid = "&max_tag_id=$max_id";
    
 } 
 //echo "https://api.instagram.com/v1/tags/GoodDeedMarathon/media/recent/?access_token=$access_token$maxid";
