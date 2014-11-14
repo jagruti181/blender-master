31586e6358369a5b5684984e27d33f8259a3e743
wohlig <chintan@wohlig.com>
2014-11-15 00:07:44 +0530

    Some minute changes

--- application/views/website/index.php
+++ application/views/website/index.php
@@ -640,7 +640,7 @@ the Blenders Pride Fashion Tour 2014 with <span class="gold">#BPMYSTYLE</span></
 
                         </div>
 
-                        <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/bpft2014" data-widget-id="531415357831913472">Tweets by @bpft2014</a>
+                        <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/hashtag/bpft2014" data-widget-id="533327175592595456">#bpft2014 Tweets</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
                     </div>
 
