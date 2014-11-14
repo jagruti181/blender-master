fe4fd5d51d6d2ee06a0348636abaedc872478df7
Tushar <Tushar@Tushars-MacBook-Pro.local>
2014-11-14 17:32:44 +0530

    Footer break added

--- application/views/website/contentfooter.php
+++ application/views/website/contentfooter.php
@@ -4,7 +4,7 @@
                 <p> <a href="<?php echo site_url('website/termscondition');?>">Terms &amp; Conditions</a>
                     <a>Privacy Policy</a> 
                 </p>
-                <p class="comp-text">Contact us: River (An Independent Creative Agency), 269 Udyog Vihar, Phase II, Gurgaon, Haryana <br> © 2014 Blenders Pride Limited. All rights reserved</p>
+                <p class="comp-text">Contact us: River (An Independent Creative Agency), 269 Udyog Vihar, Phase II, Gurgaon, Haryana </p> <p class="comp-text">© 2014 Blenders Pride Limited. All rights reserved</p>
             </div>
         </div>
     </div>
