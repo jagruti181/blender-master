e953f6ecc8dd6e88c26c8242021222077faf33e3
wohlig <chintan@wohlig.com>
2014-11-18 17:11:48 +0530

    yes removed it
    yes removed it

--- old/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $(".section").css("min-height",$(window).height());
-    $( window ).resize(function() {
-        $(".section").css("min-height",$(window).height());
-    });
-});
